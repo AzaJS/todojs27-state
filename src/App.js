98053bac53780310b031e3e34dd507c6cdd6a284
unknown <aziretbp001@gmail.com>
2022-12-08 12:46:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AddTodo from "./components/AddTodo/AddTodo";
+import TodoList from "./components/TodoList/TodoList";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  function handleTask(newObj) {
+    let newTodos = [...todos]; // копирует занчения предыдущего состояния
+    newTodos.push(newObj); // в скопированный массив пушим новый таск
+    setTodos(newTodos); // меняем состояние тудус
+  }
+  // ! функция на изменение статуса
+  function changeStatus(id) {
+    const newTodos = todos.map((item) => {
+      if (item.id === id) {
+        // сравнивает по айди
+        item.status = !item.status; //переворачивает значение статус у этого объекта
+      }
+      return item; // возвращает измененный объект в новый массив
+    });
+    setTodos(newTodos); // меняем старое состояние на новое
+  }
+  console.log(todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* отображаем компонент AddTodo и передаем туда данные из App */}
+      <AddTodo handleTask={handleTask} todos={todos} setTodos={setTodos} />
+      <TodoList changeStatus={changeStatus} todos={todos} />
     </div>
   );
-}
+};
 
 export default App;
