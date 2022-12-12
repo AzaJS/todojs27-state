facd57c919981d603bce07e6a7da537e248b2897
unknown <aziretbp001@gmail.com>
2022-12-12 13:24:03 +0600

    finished crud

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import Color from "./components/Color";
 import Like from "./components/Like";
 import RenderPic from "./components/RenderPic";
 import TodoList from "./components/TodoList/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
+  // ! переменная для хранения и отображения всех тасков
   const [todos, setTodos] = useState([]);
+
+  // ! состояние для изменения, (хранится тот таск, который будем менять)
+  const [editTodo, setEditTodo] = useState({});
+
+  // ! функция для добавления
   function handleTask(newObj) {
     let newTodos = [...todos]; // копирует занчения предыдущего состояния
     newTodos.push(newObj); // в скопированный массив пушим новый таск
@@ -23,15 +30,51 @@ const App = () => {
     });
     setTodos(newTodos); // меняем старое состояние на новое
   }
-  console.log(todos);
+
+  // ! удаление по айди
+  function deleteTodo(id) {
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos); // меняете состояние и перерисовываете компонент
+  }
+
+  // ! функция для того чтобы получить один таск
+  function handleEdit(index) {
+    setEditTodo(todos[index]);
+  }
+
+  // ! функция на изменение
+  const handleEditSave = (newTask) => {
+    // принимает новый объект
+    let newTodos = todos.map((item) => {
+      // перебирает массив состояния todos, и возвращает новый массив
+      if (item.id == newTask.id) {
+        // сравнивает если айди нового таска равен айди какого-нибудь элемента в массиве
+        return newTask; // возвращатся новый объект
+      }
+      return item; // если проверка не проходит, старый объект не меняется
+    });
+    setTodos(newTodos); // меняете состояние и перерисовываете компонент
+  };
+
+  console.log(editTodo);
   return (
     <div>
       {/* отображаем компонент AddTodo и передаем туда данные из App */}
-      {/* <AddTodo handleTask={handleTask} todos={todos} setTodos={setTodos} />
-      <TodoList changeStatus={changeStatus} todos={todos} />
-      <Color /> */}
-      <Like />
-      <RenderPic />
+      <AddTodo handleTask={handleTask} todos={todos} setTodos={setTodos} />
+      <TodoList
+        changeStatus={changeStatus}
+        todos={todos}
+        deleteTodo={deleteTodo}
+        handleEdit={handleEdit}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo}
+        handleEditSave={handleEditSave}
+      />
+      {/* <Color /> */}
+      {/* <Like />
+      <RenderPic /> */}
     </div>
   );
 };
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import ModalEdit from "../ModalEdit";
 import "./TodoList.css";
 
-const TodoList = ({ todos, changeStatus }) => {
+const TodoList = ({
+  todos,
+  changeStatus,
+  deleteTodo,
+  handleEdit,
+  editTodo,
+  setEditTodo,
+  handleEditSave,
+}) => {
+  const [show, setShow] = useState(false); // состояние для отображения модалки
+
+  const handleShow = () => setShow(true); // функция для отображения
+
   let style = {
     color: "red",
     listStyleType: "none",
@@ -9,12 +22,30 @@ const TodoList = ({ todos, changeStatus }) => {
   //   console.log(changeStatus);
   return (
     <ul style={style}>
-      {todos.map((item) => (
+      {todos.map((item, index) => (
         <li key={item.id} className={item.status ? "completed" : ""}>
+          {/* условный рендеринг класса */}
           <input type="checkbox" onChange={() => changeStatus(item.id)} />
           {item.task}
+          <button onClick={() => deleteTodo(item.id)}>&times;</button>
+          <button
+            onClick={() => {
+              handleShow(); // отображаем модальное окно
+              handleEdit(index); // подставляет значение изменяемого объекта
+            }}
+          >
+            EDIT
+          </button>
         </li>
       ))}
+      <ModalEdit
+        editTodo={editTodo}
+        handleShow={handleShow}
+        show={show}
+        setShow={setShow}
+        setEditTodo={setEditTodo}
+        handleEditSave={handleEditSave}
+      />
     </ul>
   );
 };
